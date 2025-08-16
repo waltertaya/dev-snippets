eeb3fc449deeb009137bcaff2dff401c2c7b1a48
waltertaya <waltermittybecker5@gmail.com>
2025-08-16 19:26:45 +0300

    http from tcp

---
+++ httpFromTcp/go.mod
@@ -0,0 +1,3 @@
+module github.com/waltertaya/httpFromTcp
+
+go 1.24.1
---
+++ httpFromTcp/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+
+	fmt.Println("Server started on :8080")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+
+	fmt.Println("Received:\n", string(buf[:n]))
+
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 12\r\n" +
+		"\r\n" +
+		"Hello World!"
+
+	conn.Write([]byte(response))
+}
